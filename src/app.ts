45759f4fca30898e587bf74bbb71c87f7a3ffd61
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-25 16:19:35 +0100

    setup config

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,32 @@
 import express from 'express'
 import cors from 'cors'
-import userRoutes from './routes/users'  // Make sure this path is correct
-import dotenv from 'dotenv'
-dotenv.config()
+import userRoutes from './routes/users'; // Make sure this path is correct
+import config from './config/config'; // This is file to extract environment variables, use eg. config.port
 
-const app = express()
+const app = express();
 
 // CORS setup
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || '*',
-    optionsSuccessStatus: 200,
-}
+	origin: process.env.CORS_ORIGIN || '*',
+	optionsSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
-app.use(express.json())  // This line is important for parsing JSON request bodies
+app.use(cors(corsOptions));
+app.use(express.json()); // This line is important for parsing JSON request bodies
 
 // Routes
-app.use('/users', userRoutes)  // Use the user routes
+app.use('/users', userRoutes); // Use the user routes
 
 app.get('/', (req, res) => {
-    res.send('Hello, TypeScript Express on Vercel!')
-})
-
-const port = process.env.PORT || 3000
+	res.send('Hello, TypeScript Express on Vercel!');
+});
 
 if (process.env.NODE_ENV !== 'production') {
-    app.listen(port, () => {
-        console.log(
-            `Server running in ${process.env.NODE_ENV} mode on http://localhost:${port}`
-        )
-    })
+	app.listen(config.port, () => {
+		console.log(
+			`Server running in ${config.port} mode on http://localhost:${config.port}`
+		);
+	});
 }
 
 export default app
