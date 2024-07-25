78148850e6b44d5458587c553a0bb25b3941e6c1
AlexVOiceover <alex@alexvoiceover.com>
2024-07-25 16:08:36 +0100

    Fixed routes for keys

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
 
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+dotenv.config()
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key are required')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
